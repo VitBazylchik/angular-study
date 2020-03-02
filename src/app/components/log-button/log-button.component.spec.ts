d1b8b0177b5ae6f6197d1cbc30f7594c5ab620e6
Vitali Bazylchyk <Vitali_Bazylchyk@epam.com>
2020-03-02 11:58:43 +0300

    feat: fix log-button tests

--- src/app/components/log-button/log-button.component.spec.ts
+++ src/app/components/log-button/log-button.component.spec.ts
@@ -1,28 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LogButtonComponent } from './log-button.component';
-import { Component } from '@angular/core';
-import { By } from '@angular/platform-browser';
-
-@Component({
-  template: `
-    <app-log-button [btnText]="btnText"></app-log-button>
-  `
-})
-
-class LogButtonTestComponent {
-  public btnText: string = 'Test button';
-}
 
 describe('LogButtonComponent', () => {
   let component: LogButtonComponent;
   let fixture: ComponentFixture<LogButtonComponent>;
-  let testComponent: LogButtonTestComponent;
-  let fixture2: ComponentFixture<LogButtonTestComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LogButtonComponent, LogButtonTestComponent ]
+      declarations: [ LogButtonComponent ]
     })
     .compileComponents();
   }));
@@ -32,20 +18,15 @@ describe('LogButtonComponent', () => {
     component = fixture.componentInstance;
   });
 
-  beforeEach(() => {
-    fixture2 = TestBed.createComponent(LogButtonTestComponent);
-    testComponent = fixture2.componentInstance;
-  });
-
   it('should create', () => {
     fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 
   it('should paste right text', () => {
-    fixture2.detectChanges();
-    const expectedText: string = 'Test button';
-    const button: HTMLElement = fixture2.nativeElement.querySelector(".log-button");
-    expect(button.innerText).toEqual(expectedText);
+    const button = fixture.nativeElement.querySelector('.log-button');
+    component.btnText = 'Button text';
+    fixture.detectChanges();
+    expect(button.innerText).toEqual('Button text');
   })
 });
