25ad6c9b3acb94b32eb741cddab4100ffdb93081
Vitali Bazylchyk <Vitali_Bazylchyk@epam.com>
2020-03-02 11:19:30 +0300

    feat: add unit tests to course and log-button components

--- src/app/components/course/course.component.spec.ts
+++ src/app/components/course/course.component.spec.ts
@@ -1,25 +1,54 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CourseComponent } from './course.component';
+import { Component, DebugElement } from '@angular/core';
+import { Course } from 'src/app/models/course';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `
+    <app-course 
+    [course]="course" 
+    (delete)="onDelete($event)"
+  ></app-course>
+  `
+})
+
+class TestCourseComponent {
+  public course: Course = {
+    id: 100,
+    title: 'Hello',
+    creationDate: Date.now(),
+    duration: 50,
+    description: 'gggggggagfas',
+  };
+  public id: number;
+  public onDelete(id: number) {
+    this.id = id;
+  }
+}
 
 describe('CourseComponent', () => {
-  let component: CourseComponent;
-  let fixture: ComponentFixture<CourseComponent>;
+  let component: TestCourseComponent;
+  let fixture: ComponentFixture<TestCourseComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CourseComponent ]
+      declarations: [ CourseComponent, TestCourseComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CourseComponent);
+    fixture = TestBed.createComponent(TestCourseComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should raise right id', () => {
+    fixture.detectChanges();
+    const expectedId = 100;
+    const deleteBtn: DebugElement = fixture.debugElement.query(By.css('.delete-btn'));
+    deleteBtn.triggerEventHandler('click', null);
+    expect(component.id).toEqual(expectedId);
+  })
 });
--- src/app/components/log-button/log-button.component.spec.ts
+++ src/app/components/log-button/log-button.component.spec.ts
@@ -1,14 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LogButtonComponent } from './log-button.component';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `
+    <app-log-button [btnText]="btnText"></app-log-button>
+  `
+})
+
+class LogButtonTestComponent {
+  public btnText: string = 'Test button';
+}
 
 describe('LogButtonComponent', () => {
   let component: LogButtonComponent;
   let fixture: ComponentFixture<LogButtonComponent>;
+  let testComponent: LogButtonTestComponent;
+  let fixture2: ComponentFixture<LogButtonTestComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LogButtonComponent ]
+      declarations: [ LogButtonComponent, LogButtonTestComponent ]
     })
     .compileComponents();
   }));
@@ -16,10 +30,22 @@ describe('LogButtonComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LogButtonComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+  });
+
+  beforeEach(() => {
+    fixture2 = TestBed.createComponent(LogButtonTestComponent);
+    testComponent = fixture2.componentInstance;
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should paste right text', () => {
+    fixture2.detectChanges();
+    const expectedText: string = 'Test button';
+    const button: HTMLElement = fixture2.nativeElement.querySelector(".log-button");
+    expect(button.innerText).toEqual(expectedText);
+  })
 });
